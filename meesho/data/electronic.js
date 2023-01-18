f7c7df8bc2f7c3de93cdcc4492dc2c07ff645ee5
khushboo787 <adhirajha204@gmail.com>
2023-01-18 13:31:38 +0530

    uploaded my project file now

---
+++ meesho/data/electronic.js
@@ -0,0 +1,13 @@
+const Electronics = [
+    {
+        heading : "Mobile & Accessories",
+        data : ["All Mobile & Accessories","Smartwatches","Mobile Holders","Mobile cases and covers"]
+    },
+    {
+        heading : "Appliances",
+        data : ["All Appliances","Grooming","Home Appliances"]
+    },
+]
+
+export default Electronics
+
